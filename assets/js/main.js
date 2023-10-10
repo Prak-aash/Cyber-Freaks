df3c942e2f020b315fb0c65f87689209a026b9ac
Blink ! <76958559+David-Blink@users.noreply.github.com>
2023-10-10 22:20:02 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,3 +40,19 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+
+const counters = document.querySelectorAll(".counter");
+counters.forEach((counter) => {
+counter.innerText = "0";
+const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 200;
+    if (count < target) {
+    counter.innerText = `${Math.ceil(count + increment)}`;
+    setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
+};
+updateCounter();
+});
